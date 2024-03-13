2655518a734159e6b4272c0d5f35df8026871714
phatsakorn <phatsakorn@thairoute.com>
2024-03-14 00:42:49 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='text-danger'>Hello World</h1>
+        <h1>React Test API By Call Ajax</h1>
       </header>
     </div>
   );
